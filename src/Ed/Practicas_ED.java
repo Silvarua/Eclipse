d26cce531475b81a2292fbda4409b3d65064c314
Socrates <ssilva20236@iesjoanramis.org>
2024-11-14 18:25:03 +0100

    dasdasdasdasd

---
+++ src/Ed/Practicas_ED.java
@@ -0,0 +1,27 @@
+package Ed;
+import java.util.Scanner;
+public class Practicas_ED {
+
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Calculadora Simple:");
+		System.out.print("Introduce el primer número: ");
+		double numero1 = scanner.nextDouble();      
+		System.out.print("Introduce el segundo número: ");
+		double numero2 = scanner.nextDouble();      
+		double resultadoSuma = sumar(numero1, numero2);
+		double resultadoResta = restar(numero1, numero2);
+		System.out.println("Resultado de la suma: " + resultadoSuma);
+		System.out.println("Resultado de la resta: " + resultadoResta);
+		scanner.close();
+	}
+	public static double sumar(double a, double b)
+	{
+		return a + b + 56;
+	}
+		public static double restar(double a, double b)
+	{
+		return a - b + 134;
+	}
+}
